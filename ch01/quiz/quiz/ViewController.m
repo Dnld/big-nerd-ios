3d524dc4b051eb7a83be0f39cda72c5100dd322f
Dnld <steinert.donald@gmail.com>
2015-06-01 11:18:22 -0400

    added init method to create Q's and A's; implemented showQuestion and showAnswer

--- ch01/quiz/quiz/ViewController.m
+++ ch01/quiz/quiz/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
 @property (nonatomic, weak) IBOutlet UILabel *questionLabel;
 @property (nonatomic, weak) IBOutlet UILabel *answerLabel;
 
@@ -17,14 +22,55 @@
 
 @implementation ViewController
 
+- (instancetype) initWithNibName:(NSString *)nibNameOrNil
+                          bundle:(NSBundle *)nibBundleOrNil
+{
+    // call the init method implemented by the super class
+    self = [super initWithNibName:nibNameOrNil
+                           bundle:nibBundleOrNil];
+    
+    if (self) {
+        // create two arrays filled with questions and answers
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7 + 7?",
+                           @"What is the capital of Vermont?"];
+        
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
+    
+    // return the address of the new object
+    return self;
+}
+
 - (IBAction)showQuestion:(id)sender
 {
+    // step to the next question
+    self.currentQuestionIndex++;
     
+    // go back to first question if user is past last question
+    if (self.currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    
+    // get the string at the index in the array
+    NSString *question = self.questions[self.currentQuestionIndex];
+    
+    // display the question in the question label
+    self.questionLabel.text = question;
+    
+    // reset the answer label
+    self.answerLabel.text = @"???";
 }
 
 - (IBAction)showAndwer:(id)sender
 {
+    // what is the answer to the current question
+    NSString *answer = self.answers[self.currentQuestionIndex];
     
+    // display the answer in the answer label
+    self.answerLabel.text = answer;
 }
 
 @end
