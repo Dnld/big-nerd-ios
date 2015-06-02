ba2d79e7a5ac6fa5ec5ae300a96a0476584520b2
Dnld <steinert.donald@gmail.com>
2015-06-02 13:43:56 -0400

    reimplemented draw rect in BNRHypnosisView to draw circles

--- ch04/Hypnosister/Hypnosister/BNRHypnosisView.m
+++ ch04/Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -10,4 +10,32 @@
 
 @implementation BNRHypnosisView
 
+- (void)drawRect:(CGRect)rect
+{
+    CGRect bounds = self.bounds;
+    
+    // figure out the center of the rect
+    CGPoint center;
+    center.x = bounds.origin.x  + bounds.size.width / 2.0;
+    center.y = bounds.origin.y  + bounds.size.height / 2.0;
+    
+    // circle will be as lare as can fit in the window
+    float radius = MIN(bounds.size.width, bounds.size.height) / 2.0;
+    
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    
+    // defines the path of the line
+    [path addArcWithCenter:center
+                    radius:radius
+                startAngle:0
+                  endAngle:M_PI *2.0
+                 clockwise:YES];
+    
+    path.lineWidth = 10.0;
+    [[UIColor lightGrayColor] setStroke];
+    
+    // draws the line
+    [path stroke];
+}
+
 @end
