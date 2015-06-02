ff04aa8dfbbc748a36831ccfb910d5e375d6cb4c
Dnld <steinert.donald@gmail.com>
2015-06-02 13:10:17 -0400

    created BNRHypnosisView and added first two subviews

---
+++ ch04/Hypnosister/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Donald Steinert on 6/2/15.
+//  Copyright (c) 2015 Donald Steinert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
---
+++ ch04/Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Donald Steinert on 6/2/15.
+//  Copyright (c) 2015 Donald Steinert. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+@end
